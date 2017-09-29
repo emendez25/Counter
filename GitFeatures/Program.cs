b1cb69f89044a6d7eb0d4dc1ed0932f8d817e76d
Ernesto Mendez <ernesto.mendez@anderson.ucla.edu>
2017-09-29 10:09:48 -0700

    Updating my count to be 1-5.

--- GitFeatures/Program.cs
+++ GitFeatures/Program.cs
@@ -11,7 +11,7 @@ namespace GitFeatures
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
-            for (int i = 1; i < 11; i++)
+            for (int i = 1; i < 6; i++)
             {
                 Console.WriteLine(i);
             }
